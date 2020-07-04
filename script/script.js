fbcca2a8ef507b26e726c07478af8c5a65f6084f
Konrad-Konrad <67336690+Konrad-Konrad@users.noreply.github.com>
2020-07-04 17:15:44 +0200

    tasks adding

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@
     const tasksList = [
         {
             content: "sample task",
-            done: false,
+            done: true,
         },
         {
             content: "another sample task",
@@ -12,20 +12,47 @@
 
     const renderList = () => {
         let htmlString = "";
+
         for (const task of tasksList) {
             htmlString += `
-            <li>
+            <li ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
             ${task.content}
             </li>
              `;
-        } 
-        document.querySelector(".js-tasksList").innerHTML  = htmlString
+        }
+        document.querySelector(".js-tasksList").innerHTML = htmlString;
     };
 
-    const init = () => {
+    const addNewTask = (newTaskContent) => {
+        tasksList.push({
+            content: newTaskContent,
+        });
+
         renderList();
 
     };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
+
+    const init = () => {
+
+        renderList();
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("sumbit", onFormSubmit);
+
+    };
+
     init();
 
 }
